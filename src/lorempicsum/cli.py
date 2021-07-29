2940fd10e8c277fad21e2901be5ece9315e4ecf4
Erik Håkansson <erik.hakansson@softhouse.se>
2021-07-29 17:50:19 +0200

    final fix for import hell

--- src/lorempicsum/cli.py
+++ src/lorempicsum/cli.py
@@ -1,8 +1,7 @@
 import argparse
-from engine import verify_valid_directory, download_images
 
 parser = argparse.ArgumentParser(description='CLI tool for complimenting image classification')
-parser.add_argument('-f', '--folder', default='images/', help='Specify the path of a directory in which to store photos')
+parser.add_argument('-f', '--folder', default='images', help='Specify the path of a directory in which to store photos')
 parser.add_argument('-n', '--number', help='Specify how many images you want to download', type=int)
 parser.add_argument('-s', '--seed', type=int,
     help='Used to ensure the same images are downloaded. Useful for reproduction of experiements and datasets')
@@ -11,6 +10,7 @@ help='The size of the downloaded image. Right now only square images are support
 args = parser.parse_args()
 
 def main():
+    from .engine import verify_valid_directory, download_images
     if not args.number or not args.seed: 
         raise RuntimeError('Please provide both an integer for the number of images to download, as well as an integer seed to ensure replication! See --help for more information on arguments.')
     verify_valid_directory(args.folder)
