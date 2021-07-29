93bb0b4e9a07d1b3a7e0a7ef99c24781056167b0
Erik Håkansson <erik.hakansson@softhouse.se>
2021-07-29 15:09:00 +0200

    copied flasks approach to -m invokation of cli

--- src/lorem_picsum/main.py
+++ src/lorem_picsum/main.py
@@ -1,9 +1,9 @@
 import engine
 
-def run_as_tool():
+def main():
     from cli import args
     engine.verify_valid_directory(args.folder)
     engine.download_images(master_seed=args.seed, n_images=args.number, image_directory=args.folder)
 
 if __name__ == '__main__':
-    run_as_tool()
+    main()
